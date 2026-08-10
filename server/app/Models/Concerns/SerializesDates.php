2d8042f2d04c393f601634dd545d8aeeb2183a81
chenjianjun <chenjianjun@moqing.com>
2026-08-10 10:33:44 +0800

    提交代码

--- server/app/Models/Concerns/SerializesDates.php
+++ server/app/Models/Concerns/SerializesDates.php
@@ -8,6 +8,6 @@ trait SerializesDates
 {
     public function serializeDate(DateTimeInterface $date): string
     {
-        return $date->format('Y:m:d H:i:s');
+        return $date->format('Y-m-d H:i:s');
     }
 }
